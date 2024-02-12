36239e735e65c9cbf052d2f033c3b157a97334d9
Muna Mahadi <munammahadi@gmail.com>
2024-02-12 21:07:37 +0200

    theming wlakthrogh

---
+++ app/src/main/java/com/example/theming/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.theming.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val OuasOrange = Color(color = 0xFFF7921E)
+val Purple200 = Color(color = 0xFFBB86FC)
+val Purple500 = Color(color = 0xFF6200EE)
+val Purple700 = Color(color = 0xFF3700B3)
+val Teal200 = Color(color = 0xFF03DAC5)
